6b46ae6e21ea2558158c8eddc0d9d1fb8c437537
Jonathan Hoffmann <jhhoffmann@yahoo.com>
2023-08-22 22:32:20 -0400

    fix reading of buttons

--- Boards/Adafruit_Arcada_PicoHomemade.h
+++ Boards/Adafruit_Arcada_PicoHomemade.h
@@ -2,13 +2,12 @@
 #define __ARCADA_PICO_HOMEMADE__
 #if defined(ARDUINO_RASPBERRY_PI_PICO) || defined(ARDUINO_RASPBERRY_PI_PICO_W)
 
-#include <Adafruit_Keypad.h>
 #include <Adafruit_ST7789.h>
 
 #define ARCADA_TFT_SPI SPI
-#define ARCADA_TFT_MISO 16 // Data in not used
-#define ARCADA_TFT_MOSI 19 // Data out, connect to DIN on screen
-#define ARCADA_TFT_SCLK 18 // Clock out
+#define ARCADA_TFT_MISO 16 // Display Data in, not used
+#define ARCADA_TFT_MOSI 19 // Display Data out, connect to DIN on screen
+#define ARCADA_TFT_SCLK 18 // Display Clock out
 
 #define ARCADA_TFT_CS 17 // Display CS Arduino pin number
 #define ARCADA_TFT_DC 20 // Display D/C Arduino pin number
@@ -40,44 +39,60 @@
 #include "arcadatype.h"
 
 class Adafruit_Arcada : public Adafruit_Arcada_SPITFT {
-    static const byte ARCADA_KEYPAD_ROWS = 4;
-    static const byte ARCADA_KEYPAD_COLS = 4;
-    // define the buttons of the keypad
-    char arcadaKeypadKeys[ARCADA_KEYPAD_ROWS][ARCADA_KEYPAD_COLS] = {
-        { 0, 0, ARCADA_BUTTONMASK_SELECT, ARCADA_BUTTONMASK_A },
-        { 0, 0, ARCADA_BUTTONMASK_START, ARCADA_BUTTONMASK_B },
-        { ARCADA_BUTTONMASK_UP, ARCADA_BUTTONMASK_RIGHT, 0, 0 },
-        { ARCADA_BUTTONMASK_LEFT, ARCADA_BUTTONMASK_DOWN, 0, 0 }
-    };
-    byte arcadaKeypadRowPins[ARCADA_KEYPAD_ROWS] = { ARCADA_KEYPAD_R1, ARCADA_KEYPAD_R2, ARCADA_KEYPAD_R3, ARCADA_KEYPAD_R4 };
-    byte arcadaKeypadColPins[ARCADA_KEYPAD_COLS] = { ARCADA_KEYPAD_C1, ARCADA_KEYPAD_C2, ARCADA_KEYPAD_C3, ARCADA_KEYPAD_C4 };
+private:
+    static const int ARCADA_KEYPAD_ROWS = 4;
+    static const int ARCADA_KEYPAD_COLS = 4;
 
-    Adafruit_Keypad* keypad;
-    uint32_t buttons;
+    // define the buttons of the keypad
+    static constexpr int arcadaKeypadKeys[ARCADA_KEYPAD_ROWS][ARCADA_KEYPAD_COLS] = {
+        { ARCADA_BUTTONMASK_A, ARCADA_BUTTONMASK_B, ARCADA_BUTTONMASK_START, ARCADA_BUTTONMASK_A },
+        { ARCADA_BUTTONMASK_START, ARCADA_BUTTONMASK_SELECT, ARCADA_BUTTONMASK_SELECT, ARCADA_BUTTONMASK_B },
+        { ARCADA_BUTTONMASK_UP, ARCADA_BUTTONMASK_RIGHT, ARCADA_BUTTONMASK_RIGHT, ARCADA_BUTTONMASK_DOWN },
+        { ARCADA_BUTTONMASK_LEFT, ARCADA_BUTTONMASK_DOWN, ARCADA_BUTTONMASK_UP, ARCADA_BUTTONMASK_LEFT }
+    };
+    static constexpr pin_size_t arcadaKeypadRowPins[ARCADA_KEYPAD_ROWS] = {
+        ARCADA_KEYPAD_R1, ARCADA_KEYPAD_R2, ARCADA_KEYPAD_R3, ARCADA_KEYPAD_R4
+    };
+    static constexpr pin_size_t arcadaKeypadColPins[ARCADA_KEYPAD_COLS] = {
+        ARCADA_KEYPAD_C1, ARCADA_KEYPAD_C2, ARCADA_KEYPAD_C3, ARCADA_KEYPAD_C4
+    };
+
+    static const unsigned int ARCADA_KEYPAD_SETTLING_DELAY = 20;
 
 public:
     Adafruit_Arcada(void) {};
 
     bool variantBegin(void)
     {
-        keypad = new Adafruit_Keypad(
-            makeKeymap(arcadaKeypadKeys), arcadaKeypadRowPins, arcadaKeypadColPins,
-            ARCADA_KEYPAD_ROWS, ARCADA_KEYPAD_COLS);
-        buttons = 0;
+        for (int i = 0; i < ARCADA_KEYPAD_COLS; i++) {
+            pinMode(arcadaKeypadColPins[i], OUTPUT);
+            digitalWrite(arcadaKeypadColPins[i], HIGH);
+        }
+
+        for (int i = 0; i < ARCADA_KEYPAD_ROWS; i++) {
+            pinMode(arcadaKeypadRowPins[i], INPUT_PULLUP);
+        }
+
         return true;
     }
 
     uint32_t variantReadButtons(void)
     {
-        keypad->tick();
+        uint32_t buttons = 0;
+        for (int i = 0; i < ARCADA_KEYPAD_COLS; i++) {
+            digitalWrite(arcadaKeypadColPins[i], HIGH);
+        }
 
-        while (keypad->available()) {
-            keypadEvent e = keypad->read();
-            if (e.bit.EVENT == KEY_JUST_PRESSED) {
-                buttons = buttons | e.bit.KEY;
-            } else if (e.bit.EVENT == KEY_JUST_RELEASED) {
-                buttons = buttons & ~e.bit.KEY;
+        for (int c = 0; c < ARCADA_KEYPAD_COLS; c++) {
+            digitalWrite(arcadaKeypadColPins[c], LOW);
+            delayMicroseconds(ARCADA_KEYPAD_SETTLING_DELAY);
+            for (int r = 0; r < ARCADA_KEYPAD_ROWS; r++) {
+                bool pressed = !digitalRead(arcadaKeypadRowPins[r]);
+                if (pressed) {
+                    buttons = buttons | arcadaKeypadKeys[r][c];
+                }
             }
+            digitalWrite(arcadaKeypadColPins[c], HIGH);
         }
 
         return buttons;
